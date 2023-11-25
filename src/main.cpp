20e57fe1b40d452ead142a0a08ae48806523c8cc
IlanMark <ilanm0309@gmail.com>
2023-11-25 17:11:32 +0200

    Put the connection and ping code in the 'DatabaseManager' class

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
-#include <bsoncxx/json.hpp>
-#include <mongocxx/client.hpp>
-#include <mongocxx/instance.hpp>
+#include "DatabaseManager.h"
 
 // If this run is NOT a test
 #ifndef TEST
@@ -12,31 +10,10 @@ int main()
 }
 #else
 int main()
-{
-    try
-    {
-        // Create an instance.
-        mongocxx::instance inst{};
-        const auto uri = mongocxx::uri{"mongodb+srv://ADMIN:Pass123@iep-db.dc3povw.mongodb.net/?retryWrites=true&w=majority"};
-        // Set the version of the Stable API on the client.
-        mongocxx::options::client client_options;
-        const auto api = mongocxx::options::server_api{mongocxx::options::server_api::version::k_version_1};
-        client_options.server_api_opts(api);
-        // Setup the connection and get a handle on the "admin" database.
-        mongocxx::client conn{ uri, client_options };
-        mongocxx::database db = conn["admin"];
-        // Ping the database.
-        const auto ping_cmd = bsoncxx::builder::basic::make_document(bsoncxx::builder::basic::kvp("ping", 1));
-        db.run_command(ping_cmd.view());
-        std::cout << "Pinged your deployment. You successfully connected to MongoDB!" << std::endl;
-    }
-    catch (const std::exception& e)
-    {
-        // Handle errors
-        std::cout<< "Exception: " << e.what() << std::endl;
-    }
-    
-    
+{   
+    DatabaseManager dbManager;
+    dbManager.pingDatabase();
+
     return 0;
 }
 #endif
